28fb3440b20bc50de43402b17e3e3beca3a5c621
Sohan Karfa <sohan100karfa@gmail.com>
2025-10-26 20:51:45 +0000

    payment info is not visible

--- src/components/razorpay-button.tsx
+++ src/components/razorpay-button.tsx
@@ -6,12 +6,14 @@ import Script from 'next/script';
 export function RazorpayButton() {
   return (
     <>
-      <Script
-        id="razorpay-checkout-js"
-        src="https://checkout.razorpay.com/v1/payment-button.js"
-        data-payment_button_id="pl_RYEovtgtee8sZp"
-      />
-      <form></form>
+      <form>
+          <Script
+            id="razorpay-checkout-js"
+            src="https://checkout.razorpay.com/v1/payment-button.js"
+            data-payment_button_id="pl_RYEovtgtee8sZp"
+            async
+          />
+      </form>
     </>
   );
 }
