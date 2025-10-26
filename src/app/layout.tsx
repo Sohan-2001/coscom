28d1c395b57e8b25b03b582a0d66f998a305f781
Sohan Karfa <sohan100karfa@gmail.com>
2025-10-26 10:19:45 +0000

    use Arial font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Belleza&family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+      <head />
       <body className={cn('font-body antialiased min-h-screen')}>
         {children}
         <Toaster />
