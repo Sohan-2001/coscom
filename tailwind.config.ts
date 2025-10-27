3e64f28a99071445023c9dd2818502bbc624092c
Sohan Karfa <sohan100karfa@gmail.com>
2025-10-27 06:18:44 +0000

    use ARIEL font all over the website

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Arial', 'sans-serif'],
+        headline: ['Arial', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
